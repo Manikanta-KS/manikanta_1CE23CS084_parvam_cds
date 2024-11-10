fda213d19c809fd0ccce1e3c04fa476d5a96df0d
Manikanta-KS <ksmanikanta1204@gmail.com>
2024-11-10 15:39:09 +0530

    Add files via upload

---
+++ nov 10/singly.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+struct node{
+    int data;struct node*next;
+
+};
+struct node *head=NULL,*tail=NULL;
+void create(){
+    struct node *newnode=(struct node*) malloc(sizeof(struct node));
+    int value;
+    printf("enter data:");
+    scanf("%d",&value);
+    newnode->data=value;
+    newnode->next=NULL;
+    if(head==NULL){
+        head=tail=newnode;
+    }
+    else{
+        tail->next=newnode;
+        tail=newnode;
+
+    }
+}
+void insert_at_begin(){
+    struct node *newnode=(struct node*)
+    malloc(sizeof(struct node));
+    int value;
+    printf("enter data:" );
+scanf("%d",&value);
+newnode->data=value;
+newnode->next=NULL;
+newnode->next=head;
+head=newnode;
+
+}
+void insert_at_end(){
+    struct node *newnode=(struct node*)
+    malloc(sizeof(struct node));
+    int value;
+    printf("enter data:" );
+scanf("%d",&value);
+newnode->data=value;
+newnode->next=NULL;
+tail->next=newnode;
+tail=newnode;
+
+
+}
+void delete_at_end(){
+    struct node*temp=head;
+    head=head->next;
+    free(temp);
+}
+void delete_at_end(){
+    struct node*ptr=head;
+    while(ptr->next->next!=NULL){
+        ptr=ptr->next;
+    }
+    tail=ptr;
+    ptr=ptr->next;
+}
